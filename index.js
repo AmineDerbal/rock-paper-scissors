9dcfbff69b153c6c83c419dcea183819cb1bc9ff
Kyu931 <aminedoran31@gmail.com>
2021-10-09 17:39:08 +0100

    ui changes

--- index.js
+++ index.js
@@ -1,80 +1,162 @@
+let computerWin = 0;
+let playerWin = 0;
+let choice = ["rock", "paper", "scissor"];
+let playerSelection = "";
+let rockUrl = "images/rock.svg";
+let paperUrl = "images/paper.svg";
+let scissorUrl = "images/scissor.svg";
+
+const score = document.querySelector(".score");
+const handScore = document.querySelector(".handScore");
+const resultText = document.querySelector(".result-text");
+const restart = document.querySelector(".restart");
+restart.hidden = true;
+
+// player Score declaration
+const playerScore = document.createElement("div");
+playerScore.classList.add("playerScore");
+playerScore.textContent = `Player : ${playerWin}`;
+
+// cpuScore declaration
+const cpuScore = document.createElement("div");
+cpuScore.classList.add("cpuScore");
+cpuScore.textContent = `Cpu : ${computerWin}`;
+
+// roundCommentary declaration
+const roundCommentary = document.createElement("div");
+roundCommentary.classList.add("roundcommentary");
+
+//player hand image declaration
+const playerHand = document.createElement("img");
+playerHand.classList.add("playerHand");
+
+//cpu hand image declaration
+const cpuHand = document.createElement("img");
+cpuHand.classList.add("cpuHand");
+
+// appending player and cpu Score image and round comment in class Score
+handScore.appendChild(playerHand);
+handScore.appendChild(cpuHand);
+score.appendChild(playerScore);
+score.appendChild(roundCommentary);
+score.appendChild(cpuScore);
+
+// button function
+const btns = document.querySelectorAll(".btns button");
+btns.forEach((button) => {
+  button.addEventListener("click", () => {
+    playround(button.id);
+  });
+});
+
+// button restart
+restart.addEventListener("click", () => {
+  console.log("moi");
+  playerWin = 0;
+  computerWin = 0;
+  showScore();
+  resultText.textContent = " ";
+  roundCommentary.textContent = " ";
+  restart.hidden = true;
+  btns.forEach((button) => {
+    button.disabled = false;
+  });
+});
+
+// Show the actuel score
+function showScore() {
+  playerScore.textContent = `Player : ${playerWin}`;
+  cpuScore.textContent = `Cpu : ${computerWin}`;
+}
+// check player hand with the cpu and display the result
 function playround(playerSelection) {
   let computerSelection = computerPlay();
-  console.log("the hand of the player is : " + playerSelection.toLowerCase());
-  console.log("the hand of the cpu is : " + computerSelection);
-  switch (playerSelection.toLowerCase()) {
+
+  switch (playerSelection) {
     case "rock":
       if (computerSelection == "scissor") {
-        console.log("You win rock beat scissor");
+        roundCommentary.textContent = "You Win";
+        playerHand.src = rockUrl;
+        cpuHand.src = scissorUrl;
         playerWin++;
+        showScore();
       } else if (computerSelection == "paper") {
-        console.log("You lose paper beat rock");
+        roundCommentary.textContent = "You Lose";
+        playerHand.src = rockUrl;
+        cpuHand.src = paperUrl;
         computerWin++;
+        showScore();
       } else {
-        console.log("It's a tie " + computerSelection + " against itself !!");
+        roundCommentary.textContent = "It's A Tie!";
+        playerHand.src = rockUrl;
+        cpuHand.src = rockUrl;
       }
       break;
     case "scissor":
       if (computerSelection == "paper") {
-        console.log("You win scissor beat paper");
+        roundCommentary.textContent = "You Win";
+        playerHand.src = scissorUrl;
+        cpuHand.src = paperUrl;
         playerWin++;
+        showScore();
       } else if (computerSelection == "rock") {
-        console.log("You lose rock beat scissor");
+        roundCommentary.textContent = "You Lose";
+        playerHand.src = scissorUrl;
+        cpuHand.src = rockUrl;
         computerWin++;
+        showScore();
       } else {
-        console.log("It's a tie " + computerSelection + " against itself !!");
+        roundCommentary.textContent = "It's A Tie!";
+        playerHand.src = scissorUrl;
+        cpuHand.src = scissorUrl;
       }
       break;
     case "paper":
       if (computerSelection == "rock") {
-        console.log("You win paper beat rock");
+        roundCommentary.textContent = "You Win";
+        playerHand.src = paperUrl;
+        cpuHand.src = rockUrl;
         playerWin++;
+        showScore();
       } else if (computerSelection == "scissor") {
-        console.log("You lose scissor beat paper");
+        roundCommentary.textContent = "You Lose";
+        playerHand.src = paperUrl;
+        cpuHand.src = scissorUrl;
         computerWin++;
+        showScore();
       } else {
-        console.log("It's a tie " + computerSelection + " against itself !!");
+        roundCommentary.textContent = "It's A Tie!";
+        playerHand.src = paperUrl;
+        cpuHand.src = paperUrl;
       }
       break;
   }
+  ScoreCheck();
 }
 
+// return the cpu hand randomly
 function computerPlay() {
   let rand = Math.floor(Math.random() * choice.length);
   return choice[rand];
 }
 
-function playerPlay() {
-  player = " ";
-  while (true) {
-    player = prompt("What is your hand ?");
-    if (
-      player.toLowerCase() == "rock" ||
-      player.toLowerCase() == "paper" ||
-      player.toLowerCase() == "scissor"
-    ) {
-      console.log("You choose " + player);
-      break;
-    } else {
-      console.log("try Again !");
-    }
-  }
-
-  return player;
+// check the progress and display the final result
+function ScoreCheck() {
+  if (playerWin == 5) {
+    roundCommentary.textContent = "Game Over";
+    resultText.textContent = "You Win this game";
+    restart.hidden = false;
+    restart.disabled = false;
+    btns.forEach((button) => {
+      button.disabled = true;
+    });
+  } else if (computerWin == 5) {
+    roundCommentary.textContent = "Game Over";
+    resultText.textContent = "You lose this game , Try Again";
+    restart.hidden = false;
+    restart.disabled = false;
+    btns.forEach((button) => {
+      button.disabled = true;
+    });
+  } else return;
 }
-let choice = ["rock", "paper", "scissor"];
-let playerSelection = "";
-
-let computerWin, playerWin;
-function game() {
-  computerWin = 0;
-  playerWin = 0;
-  for (i = 0; i < 5; i++) {
-    console.log("round number ", i + 1);
-    playerSelection = playerPlay();
-    playround(playerSelection);
-    console.log("Player = " + playerWin);
-    console.log("Computer = " + computerWin);
-  }
-}
-game();
---
+++ q
@@ -0,0 +1,11 @@
+[33mcommit 927c1b06020d64db114d9536069078aa4511e1f0[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/rps-ui[m[33m, [m[1;31morigin/master[m[33m, [m[1;32mrps-ui[m[33m)[m
+Author: Kyu931 <aminedoran31@gmail.com>
+Date:   Thu Oct 7 12:42:21 2021 +0100
+
+    My Second commit
+
+[33mcommit 8cae6e05c42e1ec939d66e6837281a2297cc896e[m
+Author: Kyu931 <aminedoran31@gmail.com>
+Date:   Thu Oct 7 11:23:48 2021 +0100
+
+    My first Commit
